5024c0708aff7804516db86c6e5d9f2d94fec11e
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2021-10-02 01:37:56 +0000

    Initial commit

---
+++ play/script.js
@@ -0,0 +1,15 @@
+import { letters, numbers } from "./variables.js";
+
+console.log(numbers)
+
+function q(...args){
+  return document.querySelector(...args);
+}
+
+function id(...args){
+  return document.getElementById(...args);
+}
+
+for(let i of numbers){
+  
+}
---
+++ play/variables.js
@@ -0,0 +1,4 @@
+const letters = ["a", "b", "c", "d", "e", "f", "g"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export { letters, numbers };
